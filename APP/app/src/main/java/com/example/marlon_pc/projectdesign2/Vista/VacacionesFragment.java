cfea6670a082ac3f98e211893a620bd3eeafcc49
skylar80004 <josedavidvm04@hotmail.com>
2018-11-21 13:32:03 -0600

    Boton Funcional en Vista Vacaciones

--- APP/app/src/main/java/com/example/marlon_pc/projectdesign2/Vista/VacacionesFragment.java
+++ APP/app/src/main/java/com/example/marlon_pc/projectdesign2/Vista/VacacionesFragment.java
@@ -1,28 +1,36 @@
 package com.example.marlon_pc.projectdesign2.Vista;
 
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
+import com.example.marlon_pc.projectdesign2.Controlador.Controlador;
 import com.example.marlon_pc.projectdesign2.R;
 
-public class VacacionesFragment extends Fragment {
+public class VacacionesFragment extends Fragment  {
+
     private View rootView;
-    private static final String[] periodos = {"Provincias", "Cantones", "Distritos"};
+    private static final String[] periodos = {"Verano","Mediados","SemanaSanta"};
     private static final String[] anhos = {"2012", "2013", "2014"};
     private Spinner spinner_periodos;
     private Spinner spinner_anhos;
+    private Controlador controlador;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         rootView = inflater.inflate(R.layout.fragment_vacaciones, container,false);
 
         ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Vacaciones");
@@ -39,8 +47,48 @@ public class VacacionesFragment extends Fragment {
         adapter_anhos.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         spinner_anhos.setAdapter(adapter_anhos);
 
+
+        this.controlador = new Controlador();
+
+        // Boton de consulta
+        final Button button = (Button) rootView.findViewById(R.id.buttonConsultaVacaciones);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                /* Dialog
+                String mensaje = "hola";
+                String titulo = "Titulo de barra ";
+                // 1. Instantiate an AlertDialog.Builder with its constructor
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage(mensaje)
+                        .setTitle(titulo);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                */
+
+
+                System.out.println("Hola");
+                //controlador.enviarConsulta();
+
+
+
+
+
+            }
+        });
+
+
+
         return rootView;
     }
+
+
+
+
+
+
+
 
 
 
