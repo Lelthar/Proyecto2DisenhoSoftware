2b4cb09c2c0a5f39334b1821df5aca9c6b7b268e
lelthar <geraldm1998@hotmail.com>
2018-11-21 21:08:20 -0600

    Se agregó el mapa

--- APP/app/src/main/java/com/example/marlon_pc/projectdesign2/Vista/MapaConsultaDinamica.java
+++ APP/app/src/main/java/com/example/marlon_pc/projectdesign2/Vista/MapaConsultaDinamica.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.media.AudioRecord;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -15,9 +16,16 @@ import android.util.Log;
 import com.example.marlon_pc.projectdesign2.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapaConsultaDinamica extends AppCompatActivity implements  OnMapReadyCallback{
 
@@ -26,6 +34,9 @@ public class MapaConsultaDinamica extends AppCompatActivity implements  OnMapRea
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
+
+    private FusedLocationProviderClient fusedLocationProviderCliente;
 
     private boolean mLocationPermissionsGranted = false;
     private GoogleMap mMap;
@@ -33,6 +44,17 @@ public class MapaConsultaDinamica extends AppCompatActivity implements  OnMapRea
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        if (mLocationPermissionsGranted) {
+            getDeviceLocation();
+
+            if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+            //mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+        }
     }
 
     @Override
@@ -43,7 +65,33 @@ public class MapaConsultaDinamica extends AppCompatActivity implements  OnMapRea
         getLocationPermission();
 
 
+    }
 
+    private void getDeviceLocation() {
+        fusedLocationProviderCliente = LocationServices.getFusedLocationProviderClient(this);
+
+        try {
+            if (mLocationPermissionsGranted) {
+                final Task location = fusedLocationProviderCliente.getLastLocation();
+               location.addOnCompleteListener(new OnCompleteListener() {
+                   @Override
+                   public void onComplete(@NonNull Task task) {
+                       if (task.isSuccessful()) {
+                           Location currentLocation = (Location) task.getResult();
+                           moveCamera(new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude()),DEFAULT_ZOOM );
+                       } else {
+
+                       }
+                   }
+               });
+            }
+        }catch (SecurityException e){
+
+        }
+    }
+
+    private void moveCamera (LatLng latLng, float zoom) {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
     }
 
     public void initMap() {
@@ -58,6 +106,7 @@ public class MapaConsultaDinamica extends AppCompatActivity implements  OnMapRea
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(),FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             if (ContextCompat.checkSelfPermission(this.getApplicationContext(),COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 mLocationPermissionsGranted = true;
+                initMap();
             } else {
                 ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSION_REQUEST_CODE);
             }
